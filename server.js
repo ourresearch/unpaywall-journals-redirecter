9d3af9acc79f42f128be7f1f101c03fefff4c264
Jason Priem <jp@jasonpriem.org>
2020-05-16 15:17:04 -0700

    remove most dependencies.

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+
+let app = express();
+
+
+// this was helpful for configs:
+// https://scotch.io/tutorials/creating-a-single-page-todo-app-with-node-and-angular
+app.use(serveStatic(__dirname + "/dist"));
+
+
+
+app.get('*', function (req, res) {
+    res.redirect("https://get-unsub.org" + req.path)
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log('Listening on port ' + port)
+});
